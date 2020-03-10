50c786a863f6343d6c784d09c77be6fa4cb4fd97
Reina <r.clarkemitchell@gmail.com>
2020-03-10 05:36:30 -0400

    Original Repo not working. Finished the title slides withh the typed.…
    js 3/6. Finished about me section 3/7. Finished the technical skills section and stats 3/8. Working on contact me. Deciding what to do for blog posts.

---
+++ JS/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+	$('#slides').superslides({
+		animation: 'fade',
+		pagination: false,
+		play: 5000,
+	})
+
+	// Typed.js by www.mattboldt.com
+	var text = new Typed('.typed', {
+		strings: ['Full Stack Developer.', 'Creative.', 'Productivity Enthusiast.'],
+		// !My original
+		typeSpeed: 60,
+		backSpeed: 40,
+		bindInputFocusEvents: true,
+		loop: true,
+		loopCount: Infinity,
+		cursorChar: '|',
+		backDelay: 2000,
+		startDelay: 1000,
+	})
+
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		items: 4,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			480: {
+				items: 2,
+			},
+			768: {
+				items: 3,
+			},
+			938: {
+				items: 4,
+			},
+		},
+	})
+
+	let skillTop = $('.skillsSection').offset().top
+	let statTop = $('.statsSection').offset().top
+	let countUp = false
+	$(window).scroll(function() {
+		if (window.pageYOffset > skillTop - $(window).height() + 200) {
+			$('.chart').easyPieChart({
+				easing: 'easeInOut',
+				barColor: '#29f3c3',
+				trackColor: '#272727',
+				scaleColor: false,
+				lineWidth: 7,
+				size: 152,
+				animate: { duration: 2000, enabled: true },
+				onStep: function(from, to, percent) {
+					$(this.el)
+						.find('.percent')
+						.text(Math.round(percent))
+				},
+			})
+		}
+
+		if (!countUp && window.pageYOffset > statTop - $(window).height() + 200) {
+			$('.counter').each(function() {
+				let el = $(this)
+				let endVal = parseInt(el.text())
+				el.countup(endVal)
+			})
+			countUp = true
+		}
+	})
+})
