5c9cea6527905b9ca38a870d2eeeb3f2b1618254
Reina <r.clarkemitchell@gmail.com>
2020-03-14 03:52:30 -0400

    going to add scroll to nav bar for links

--- JS/script.js
+++ JS/script.js
@@ -87,4 +87,20 @@ $(document).ready(function() {
 		})
 		return false
 	})
+
+	const nav = $('#navigationBar')
+	const navTop = nav.offset().top
+
+	$(window).on('scroll', stickyNavigation)
+
+	function stickyNavigation() {
+		let body = $('body')
+		if ($(window).scrollTop() >= navTop) {
+			body.css('padding-top', nav.outerHeight() + 'px')
+			body.addClass('fixedNav')
+		} else {
+			body.css('padding-top', 0)
+			body.removeClass('fixedNav')
+		}
+	}
 })
