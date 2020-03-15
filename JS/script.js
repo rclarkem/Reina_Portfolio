bf395468962280cbef06e5570b132ff30702872b
Reina <r.clarkemitchell@gmail.com>
2020-03-14 23:19:39 -0400

    finished

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,14 @@
+$(window).on('load', function() {
+	$('.loader .inner').fadeOut(500, function() {
+		$('.loader').fadeOut(750)
+	})
+
+	$('.items').isotope({
+		filter: '*',
+		animationOptions: { duration: 2000, easing: 'linear', queue: false },
+	})
+})
+
 $(document).ready(function() {
 	$('#slides').superslides({
 		animation: 'fade',
@@ -69,11 +80,6 @@ $(document).ready(function() {
 		}
 	})
 	$('[data-fancybox]').fancybox()
-
-	$('.items').isotope({
-		filter: '*',
-		animationOptions: { duration: 2000, easing: 'linear', queue: false },
-	})
 
 	$('#filters a').click(function() {
 		$('#filters .current').removeClass('current')
